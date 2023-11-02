90da4a43b111055fb10e2ecdd8e1bf94d0b30794
Harsh G <harshsoftcolon1105@gmail.com>
2023-11-02 18:27:30 +0530

    fixed bugs

--- src/components/languages/AddLanguageModal.tsx
+++ src/components/languages/AddLanguageModal.tsx
@@ -112,6 +112,8 @@ const AddLanguageModal = ({
         : items
     );
     dispatch(editProjectLanguage(updatedLanguageData));
+    setIsEdit(false);
+    handleClose();
   };
 
   const handleCloseModal = () => {
